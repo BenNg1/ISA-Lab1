99e4ddb1dbf42d9b4af3820a5d603cc3e182fa38
Ben Nguyen <benc.nguyen02@gmail.com>
2026-01-18 17:54:50 -0800

    define my user strings and create the structure

---
+++ lang/messages/en/user.js
@@ -0,0 +1,6 @@
+const MSG_APP_TITLE = "Lab 1: JSON, Object Constructor, localStorage";
+const MSG_ADD_NOTE = "Add Note";
+const MSG_REMOVE_NOTE = "Remove";
+const MSG_LAST_SAVED = "Last saved at:";
+const MSG_LAST_RETRIEVED = "Last retrieved at:";
+const MSG_BACK = "Back";
